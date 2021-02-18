e404df7d72fc108148b1da82eb9ac8d87f63daea
Radu Cotisel <raducotisel@yahoo.com>
2021-02-18 21:05:26 +0200

    Initial Project Config.

---
+++ MovieAssistant/MovieAssistant/MovieAssistant/API/ApiConnector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+
+namespace MovieAssistant.API
+{
+    internal class ApiConnector
+    {
+        internal async void connect()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com/Images/%7Bfilepath%7D/Redirect?Redirect=True"),
+                Headers =
+                {
+                    { "accept", "application/json" },
+                    { "x-rapidapi-key", "7666ee6291msh2893aa2cef00efap1bc99fjsncfa4e7c79b60" },
+                    { "x-rapidapi-host", "ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+            }
+        }
+
+    }
+}
---
+++ MovieAssistant/MovieAssistant/MovieAssistant/Model/Movie.cs
@@ -0,0 +1,130 @@
+﻿using Android.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace MovieAssistant.Model
+{
+    internal class Movie
+    {
+        public Movie(string programType, string title, int year, string language, int runtime, string description, List<Contributor> contributors, Bitmap image = null)
+        {
+            this.programType = programType;
+            this.title = title;
+            this.year = year;
+            this.language = language;
+            this.runtime = runtime;
+            this.description = description;
+            this.contributors = contributors;
+            this.image = image;
+        }
+
+        #region Properties
+        public string ProgramType
+        {
+            get
+            {
+                return programType;
+            }
+            set
+            {
+                programType = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
+
+        public string Language
+        {
+            get
+            {
+                return language;
+            }
+            set
+            {
+                language = value;
+            }
+        }
+
+        public int Runtime
+        {
+            get
+            {
+                return runtime;
+            }
+            set
+            {
+                runtime = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+
+        public List<Contributor> Contributors
+        {
+            get
+            {
+                return contributors;
+            }
+            set
+            {
+                contributors = value;
+            }
+        }
+
+        public Bitmap Image
+        {
+            get
+            {
+                return image;
+            }
+            set
+            {
+                image = value;
+            }
+        }
+        #endregion
+
+        #region Fields
+        private string programType;
+        private string title;
+        private int year;
+        private string language;
+        private int runtime;
+        private Bitmap image;
+        private string description;
+        private List<Contributor> contributors;
+        #endregion
+    }
+}
