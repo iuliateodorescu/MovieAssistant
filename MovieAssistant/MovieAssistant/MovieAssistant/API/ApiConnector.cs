fb337544dc4449f891bab2ebf27380878aa68bbe
Radu Cotisel <raducotisel@yahoo.com>
2021-02-19 20:43:34 +0200

    Movie mapping started. Project cleaning done.

--- MovieAssistant/MovieAssistant/MovieAssistant/API/ApiConnector.cs
+++ MovieAssistant/MovieAssistant/MovieAssistant/API/ApiConnector.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MovieAssistant.Model;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.Net.Http;
 
 namespace MovieAssistant.API
@@ -7,25 +11,78 @@ namespace MovieAssistant.API
     {
         internal async void connect()
         {
-            var client = new HttpClient();
-            var request = new HttpRequestMessage
+            try
             {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri("https://ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com/Images/%7Bfilepath%7D/Redirect?Redirect=True"),
-                Headers =
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
                 {
-                    { "accept", "application/json" },
-                    { "x-rapidapi-key", "7666ee6291msh2893aa2cef00efap1bc99fjsncfa4e7c79b60" },
-                    { "x-rapidapi-host", "ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com" },
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com/entertainment/match/?Title=Titanic&ProgramType=Movie"),
+                    Headers =
+                {
+                    { "x-rapidapi-key", apiKey },
+                    { "x-rapidapi-host", apiHost },
                 },
-            };
-            using (var response = await client.SendAsync(request))
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+
+                    List<Movie> movies = parseMovieListJson(JObject.Parse(body));
+                }
+            }
+            catch (Java.Net.UnknownHostException)
             {
-                response.EnsureSuccessStatusCode();
-                var body = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(body);
+                Console.WriteLine("No Internet Connection");
             }
         }
 
+        private List<Movie> parseMovieListJson(JObject movieListJson)
+        {
+            List<Movie> movieList = new List<Movie>();
+
+            foreach (var movieJson in movieListJson.Value<IEnumerable<JToken>>("ProgramMatches"))
+            {
+                movieList.Add(parseMovieJson(movieJson));
+            }
+
+            return movieList;
+        }
+
+        private Movie parseMovieJson(JToken movieJson)
+        {
+            string programType = movieJson.Value<string>("ProgramType");
+            string title = movieJson.Value<string>("Title");
+            int year = movieJson.Value<int>("Year");
+            string language = movieJson.Value<string>("OriginalLanguage");
+            int runtime = movieJson.Value<int>("Runtime");
+            string description = "";
+            try
+            {
+                description = movieJson.Value<JToken>("Descriptions").First.Value<string>("Description");
+            }
+            catch (Exception) { }
+
+            List<Contributor> contributors = new List<Contributor>();
+
+            try
+            {
+                foreach (var contributor in movieJson.Value<JToken>("Contributors"))
+                {
+                    contributors.Add(new Contributor(contributor.Value<string>("PersonName"), contributor.Value<string>("Job")));
+                }
+            }
+            catch (Exception) { }
+            
+            //Bitmap image
+
+            return new Movie(programType, title, year, language, runtime, description, contributors);
+        }
+
+        #region Fields
+        private string apiKey = "7666ee6291msh2893aa2cef00efap1bc99fjsncfa4e7c79b60";
+        private string apiHost = "ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com";
+        #endregion
     }
 }
