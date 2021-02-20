09418a504294b9722c793e9f09dcd5c0bd6dd807
Iulia <iulya.teodorescu@gmail.com>
2021-02-20 18:20:13 +0200

    created tabbed page

--- MovieAssistant/MovieAssistant/MovieAssistant/Views/HistoryPage.xaml.cs
+++ MovieAssistant/MovieAssistant/MovieAssistant/Views/HistoryPage.xaml.cs
@@ -1,22 +1,20 @@
-﻿using MovieAssistant.API;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace MovieAssistant.Views
 {
-    public partial class MainPage : TabbedPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HistoryPage : ContentPage
     {
-        public MainPage()
+        public HistoryPage()
         {
             InitializeComponent();
-
-            ApiConnector connector = new ApiConnector();
-
-            connector.connect();
         }
     }
 }
--- MovieAssistant/MovieAssistant/MovieAssistant/Views/HomePage.xaml.cs
+++ MovieAssistant/MovieAssistant/MovieAssistant/Views/HomePage.xaml.cs
@@ -1,22 +1,20 @@
-﻿using MovieAssistant.API;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace MovieAssistant.Views
 {
-    public partial class MainPage : TabbedPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
     {
-        public MainPage()
+        public HomePage()
         {
             InitializeComponent();
-
-            ApiConnector connector = new ApiConnector();
-
-            connector.connect();
         }
     }
 }
